5b8b1e49d494d5b9b0bf7db81ee285170e3ef016
Zubry <zubryjs@gmail.com>
2016-06-21 20:23:48 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { Record as record } from 'immutable';
+
+
+const ColorPalette = record({
+  primary: 'white',
+  secondary: 'white',
+  tertiary: 'white',
+  quaternary: 'white',
+});
+
+export default ColorPalette;
